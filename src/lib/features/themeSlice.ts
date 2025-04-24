125e9ab7779841a505d2aa35e6c88f59f68a3160
Danil Sagaidachnyi <chairowner@yandex.ru>
2025-04-24 10:31:51 +0500

    check

--- src/lib/features/themeSlice.ts
+++ src/lib/features/themeSlice.ts
@@ -19,7 +19,7 @@ const initialState: Theme = {
   primary20: rgba('#42567a', 0.2),
   primary50: rgba('#42567a', 0.5),
   background: '#F4F5F9',
-  accent: '#3877EE',
+  accent: '#ee3838',
   accent10: rgba('#3877EE', 0.1),
   secondary: '#EF5DA8',
   light: '#ffffff',
